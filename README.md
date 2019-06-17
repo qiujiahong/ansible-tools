387a71344393bec997a309f766f7c601992c484b
qiujiahong <qiujiahongde@163.com>
2019-06-17 08:53:31 +0800

    初始化工程

---
+++ README.md
@@ -0,0 +1,5 @@
+# ANSIBLE 安装一些常用软件
+
+
+
+* [系统准备](docs/prepare.md)
---
+++ vagrant/init.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# 允许root远程登录
+sed -i 's/^#PermitRootLogin.*$/PermitRootLogin yes/g' /etc/ssh/sshd_config
+# 允许密码验证
+sed -i 's/^PasswordAuthentication.*$/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+# 允许免密远程登录
+sed -i 's/^#PubkeyAuthentication.*$/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+systemctl restart sshd.service

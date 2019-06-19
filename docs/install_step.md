f3e01d0b42e25bfa1e5a94b3e7c667d10675a1a3
qiujiahong <qiujiahongde@163.com>
2019-06-19 23:49:40 +0800

    安装步骤

--- docs/install_step.md
+++ docs/install_step.md
@@ -5,5 +5,6 @@ cd /etc/ansible
 ansible-playbook 01.prepare.yml
 ansible-playbook 02.java.yml
 ansible-playbook 05.redis.yml
+ansible-playbook 06.tomcat.yml
 ansible-playbook 99.clean.yml
 ```
--- down/download.sh
+++ down/download.sh
@@ -3,3 +3,4 @@ curl -L -C - -b "oraclelicense=accept-securebackup-cookie" -O \
  'http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz'
 wget https://cdn.mysql.com//Downloads/MySQL-5.7/mysql-5.7.26-el7-x86_64.tar.gz
 wget http://download.redis.io/releases/redis-5.0.5.tar.gz
+wget http://mirror.bit.edu.cn/apache/tomcat/tomcat-8/v8.5.42/bin/apache-tomcat-8.5.42.tar.gz

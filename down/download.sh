97d52b060105afae4e8e98d3963eece792940a74
qiujiahong <qiujiahongde@163.com>
2019-06-20 19:15:43 +0800

    添加nginx

--- down/download.sh
+++ down/download.sh
@@ -4,3 +4,4 @@ curl -L -C - -b "oraclelicense=accept-securebackup-cookie" -O \
 wget https://cdn.mysql.com//Downloads/MySQL-5.7/mysql-5.7.26-el7-x86_64.tar.gz
 wget http://download.redis.io/releases/redis-5.0.5.tar.gz
 wget http://mirror.bit.edu.cn/apache/tomcat/tomcat-8/v8.5.42/bin/apache-tomcat-8.5.42.tar.gz
+wget https://nginx.org/download/nginx-1.12.2.tar.gz

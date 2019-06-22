fe22bf61a233f4b196a139a38fc656bb9b96f70c
qiujiahong <qiujiahongde@163.com>
2019-06-22 09:29:39 +0800

    添加script

---
+++ scripts/1.installansible.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# 文档中脚本默认均以root用户执行
+# 安装 epel 源
+yum install epel-release -y
+# 安装依赖工具
+yum install git python python-pip -y
+pip install pip --upgrade -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
+pip install --no-cache-dir ansible -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
+mkdir /etc/ansible
--- scripts/scp.sh
+++ scripts/scp.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 # scp -r ./*  root@192.168.2.11:/etc/ansible
 rsync -arv --exclude ".*" ./ root@192.168.2.11:/etc/ansible

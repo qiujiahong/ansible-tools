3cd8a5dad11ff6d73b7be103a30191531562ab37
qiujiahong <qiujiahongde@163.com>
2019-06-22 09:46:37 +0800

    修改上传文件脚本

--- scripts/1.installansible.sh
+++ scripts/1.installansible.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-
 # 文档中脚本默认均以root用户执行
 # 安装 epel 源
 yum install epel-release -y
@@ -8,4 +6,3 @@ yum install epel-release -y
 yum install git python python-pip -y
 pip install pip --upgrade -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
 pip install --no-cache-dir ansible -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
-mkdir /etc/ansible

116637ab548e4591ca5b0d514af3da4efb228833
qiujiahong <qiujiahongde@163.com>
2020-06-11 23:03:21 +0800

    提交任务

--- up.sh
+++ up.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 # scp -r ./*  root@192.168.2.11:/etc/ansible
-rsync -arv --exclude ".*" ./ root@192.168.2.11:/etc/ansible
+# rsync -arv --exclude ".*" ./ root@192.168.2.11:/etc/ansible
+
+msg=${1-commit}
+
+echo $msg
+git add .
+git commit -m "$msg"
+git push origin 
+git push mayun 
